fdce5f003c6dfdd358c0d61bd4f81506d46c131c
bkaracan <burakkaracan94@gmail.com>
2024-04-16 15:27:22 +0300

    ++activation process added to AuthenticationService.

--- src/main/java/com/bkaracan/book/service/AuthenticationService.java
+++ src/main/java/com/bkaracan/book/service/AuthenticationService.java
@@ -1,10 +1,72 @@
 package com.bkaracan.book.service;
 
 import com.bkaracan.book.authentication.RegistrationRequest;
+import com.bkaracan.book.entity.Token;
+import com.bkaracan.book.entity.User;
+import com.bkaracan.book.repository.RoleRepository;
+import com.bkaracan.book.repository.TokenRepository;
+import com.bkaracan.book.repository.UserRepository;
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
 public class AuthenticationService {
 
-    public void register(RegistrationRequest request) {
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new IllegalStateException("Role USER was not initialized!"));
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .isAccountLocked(false)
+                .isEnabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
     }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        // send email
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // generate a token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiredAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for(int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
+
+
 }

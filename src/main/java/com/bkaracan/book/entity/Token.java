0a09112aec8f5034ed482dac816ed2732403e157
bkaracan <burakkaracan94@gmail.com>
2024-04-16 09:39:41 +0300

    ++token entity and repository created.

---
+++ src/main/java/com/bkaracan/book/entity/Token.java
@@ -0,0 +1,45 @@
+package com.bkaracan.book.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
+public class Token {
+
+    @Id
+    @SequenceGenerator(name = "TOKEN_ID_GENERATOR", sequenceName = "TOKEN_ID_GEN", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TOKEN_ID_GENERATOR")
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiredAt;
+
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/bkaracan/book/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package com.bkaracan.book.repository;
+
+import com.bkaracan.book.entity.Token;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    Optional<Token> findByToken(String token);
+}

96d92149267fbf038f19e92bc4b0e40fbe7d8d34
bkaracan <burakkaracan94@gmail.com>
2024-04-16 13:39:30 +0300

    ++jwt layer created.

---
+++ src/main/java/com/bkaracan/book/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.bkaracan.book.service.impl;
+
+import com.bkaracan.book.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+}

a553f86c2448d015746e956b255db2ee44934e39
= <=>
2024-04-22 10:13:49 +0300

    ++entity relationships added for all entity class.

--- src/main/java/com/bkaracan/book/entity/Feedback.java
+++ src/main/java/com/bkaracan/book/entity/Feedback.java
@@ -2,6 +2,8 @@ package com.bkaracan.book.entity;
 
 import com.bkaracan.book.entity.common.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,5 +23,9 @@ public class Feedback extends BaseEntity {
     private Double note;
 
     private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
 
 }

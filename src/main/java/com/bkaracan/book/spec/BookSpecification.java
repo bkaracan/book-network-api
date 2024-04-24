d89b25e43eb0cb8f368f75b638a55e8c00a0bca9
= <=>
2024-04-24 15:20:19 +0300

    ++SonarLint private constructor issue for BookSpecification.java fixed.

--- src/main/java/com/bkaracan/book/spec/BookSpecification.java
+++ src/main/java/com/bkaracan/book/spec/BookSpecification.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class BookSpecification {
 
+    private BookSpecification() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated!");
+    }
+
     public static Specification<Book> withOwnerId(Long ownerId) {
-        return(root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").get("id"), ownerId);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").get("id"), ownerId);
     }
 }

aedc1dd37eb177ccc1f99bf7f285e876b756dcb2
= <=>
2024-04-24 16:12:13 +0300

    ++ApplicationContext added test class.

--- src/main/java/com/bkaracan/book/dto/request/BookRequest.java
+++ src/main/java/com/bkaracan/book/dto/request/BookRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record BookRequest(
+
         Long id,
 
         @NotNull(message = "100")
--- src/test/java/com/bkaracan/book/BookNetworkApiApplicationTests.java
+++ src/test/java/com/bkaracan/book/BookNetworkApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.bkaracan.book;
 
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
-class BookNetworkApiApplicationTests {
+@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
+class BookNetworkApplicationTests {
+
+	private final ApplicationContext context;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(context, "The application context should be loaded");
 	}
-
 }

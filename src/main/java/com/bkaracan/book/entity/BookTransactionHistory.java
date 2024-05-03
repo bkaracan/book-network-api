d2af0332b48afe4b6ce80cf92af765544527abef
bkaracan <burakkaracan94@gmail.com>
2024-05-03 09:52:45 +0300

    ++BookTransactionHistory layer refactored.

--- src/main/java/com/bkaracan/book/entity/BookTransactionHistory.java
+++ src/main/java/com/bkaracan/book/entity/BookTransactionHistory.java
@@ -4,14 +4,12 @@ import com.bkaracan.book.entity.common.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
-import java.io.Serializable;
 
 @Getter
 @Setter
@@ -19,12 +17,9 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "book_transact_history")
 public class BookTransactionHistory extends BaseEntity implements Serializable {
 
-    private boolean isReturned;
 
-    private boolean isReturnApproved;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -33,4 +28,8 @@ public class BookTransactionHistory extends BaseEntity implements Serializable {
     @ManyToOne
     @JoinColumn(name = "book_id")
     private Book book;
+
+    private boolean isReturned;
+    private boolean isReturnApproved;
+
 }

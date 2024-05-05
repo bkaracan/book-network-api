ff05d45fda5b7b49b1a28988cb681722a155fd3e
= <=>
2024-05-05 22:38:36 +0300

    ++sanitizing file path.

--- src/main/java/com/bkaracan/book/service/impl/FileStorageServiceImpl.java
+++ src/main/java/com/bkaracan/book/service/impl/FileStorageServiceImpl.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 @Service
 @Slf4j
@@ -24,51 +25,50 @@ public class FileStorageServiceImpl implements FileStorageService {
     private String fileUploadPath;
 
     @Override
-    public String saveFile(@NonNull MultipartFile sourceFile, @NonNull Long userId) {
-        if (userId < 0) {
-            log.error("Invalid user ID");
-            return null;
-        }
-
-        final String sanitizedUserId = sanitizeUserId(userId);
-        final String fileUploadSubPath = "users" + File.separator + sanitizedUserId;
+    public String saveFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull Long userId) {
+        final String fileUploadSubPath = "users" + File.separator + userId;
         return uploadFile(sourceFile, fileUploadSubPath);
     }
 
-    private String sanitizeUserId(Long userId) {
-        return userId.toString();
-    }
-
     private String uploadFile(@NonNull MultipartFile sourceFile, @NonNull String fileUploadSubPath) {
-        final String finalUploadPath = fileUploadPath + File.separator + fileUploadSubPath;
-        File targetFolder = new File(finalUploadPath);
-        if (!targetFolder.exists()) {
-            boolean folderCreated = targetFolder.mkdirs();
-            if (!folderCreated) {
-                log.warn("Failed to create the target folder!");
-                return null;
-            }
+        String sanitizedFilename = sanitizeFilename(Objects.requireNonNull(sourceFile.getOriginalFilename()));
+        if (sanitizedFilename.isEmpty()) {
+            log.error("Invalid file name");
+            return null;
         }
-        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
-        String targetFilePath = finalUploadPath + File.separator + System.currentTimeMillis() + "." + fileExtension;
+
+        final String finalUploadPath = fileUploadPath + File.separator + sanitizePath(fileUploadSubPath);
+        File targetFolder = new File(finalUploadPath);
+        if (!targetFolder.exists() && !targetFolder.mkdirs()) {
+            log.warn("Failed to create the target folder!");
+            return null;
+        }
+
+        String targetFilePath = finalUploadPath + File.separator + System.currentTimeMillis() + "." + getFileExtension(sanitizedFilename);
         Path targetPath = Paths.get(targetFilePath);
         try {
             Files.write(targetPath, sourceFile.getBytes());
             log.info("File saved to " + targetFilePath);
         } catch (IOException e) {
             log.error("File was not saved!", e);
+            return null;
         }
         return null;
     }
 
+    private String sanitizeFilename(String filename) {
+        return filename.replaceAll("[^a-zA-Z0-9.\\-_]", "");
+    }
+
+    private String sanitizePath(String path) {
+        return path.replaceAll("[^a-zA-Z0-9/]", "");
+    }
+
     private String getFileExtension(String filename) {
-        if(filename == null || filename.isEmpty()) {
-            return "";
-        }
         int lastDotIndex = filename.lastIndexOf(".");
-        if(lastDotIndex == -1) {
-            return "";
-        }
+        if (lastDotIndex == -1 || lastDotIndex == 0) return "";
         return filename.substring(lastDotIndex + 1).toLowerCase();
     }
 }
